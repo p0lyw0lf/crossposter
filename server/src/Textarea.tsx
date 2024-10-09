8bca1329910a0040b73ea6592f261a355be71c86
p0lyw0lf <31190026+p0lyw0lf@users.noreply.github.com>
2024-10-08 20:36:45 -0400

    fix scroll

--- server/src/Textarea.tsx
+++ server/src/Textarea.tsx
@@ -7,13 +7,19 @@ export const Textarea: Component<
   let textarea: HTMLTextAreaElement | undefined;
   onMount(() => {
     if (!textarea) return;
+
     // Magic code from https://stackoverflow.com/a/25621277
     textarea.style.height = textarea.scrollHeight + "px";
     textarea.style.overflowY = "hidden";
 
     textarea.addEventListener("input", () => {
+      const oldScrollX = window.scrollX;
+      const oldScrollY = window.scrollY;
+
       textarea.style.height = "auto";
       textarea.style.height = textarea.scrollHeight + "px";
+
+      window.scrollTo(oldScrollX, oldScrollY);
     });
   });
 
