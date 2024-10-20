6ba0944bbc3565c5d603e0856aae0eff01f86de6
p0lyw0lf <31190026+p0lyw0lf@users.noreply.github.com>
2024-10-19 20:29:24 -0400

    wip: file upload endpoint ?

--- server/README.md
+++ server/README.md
@@ -13,6 +13,14 @@ In addition to the secrets configured previously, also add a value for
 `SERVER_SECRET` in `secrets.toml`, as well as a mapping of usernames ->
 passwords in the `server.users.<user>` dictionary.
 
+If you want to support file uploads, you'll need to add the following to
+`secrets.toml` too:
+
+- `AWS_ACCESS_KEY_ID`: from `~/.aws/credentials`
+- `AWS_SECRET_ACCESS_KEY`: from `~/.aws/credentials`
+- `AWS_REGION_NAME`: the region the bucket lives in
+- `AWS_BUCKET_NAME`: the name of the bucket
+
 Finally, from the root of the repository, run:
 
 ```bash
---
+++ server/file_upload.py
@@ -0,0 +1,51 @@
+import asyncio
+import traceback
+
+import aiofiles
+import boto3
+from sanic.response import text
+from sanic import Blueprint, Request
+
+from shared.secrets import secrets
+from .auth import login_required
+
+bp = Blueprint("file_upload")
+s3 = None
+bucket_name = None
+try:
+    s3 = boto3.client(
+        service_name="s3",
+        region_name=secrets["AWS_REGION_NAME"],
+        aws_access_key_id=secrets["AWS_ACCESS_KEY_ID"],
+        aws_secret_access_key=secrets["AWS_SECRET_ACCESS_KEY"],
+    )
+    bucket_name = secrets["AWS_BUCKET_NAME"]
+except Exception:
+    print(traceback.format_exc())
+
+
+@bp.post("/", stream=True)
+@login_required
+async def upload(request: Request):
+    if s3 is None or bucket_name is None:
+        return text("not implemented", status=501)
+
+    file_name = request.args.get("f", None)
+    if not file_name:
+        return text("file name not provided", status=400)
+
+    async with aiofiles.tempfile.NamedTemporaryFile("wb+") as f:
+        # Step 1: read streamed data into file
+        while True:
+            body = await request.stream.read()
+            if body is None:
+                break
+            await f.write(body)
+
+        # Step 2: upload file to S3
+        await asyncio.to_thread(
+            s3.upload_fileobj,
+            (f, bucket_name, file_name),
+        )
+
+    return text("ok")
