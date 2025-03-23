eb4e0575406dfbfe19035c5ed7f97859b4be04a0
PolyWolf <31190026+p0lyw0lf@users.noreply.github.com>
2025-03-23 10:43:46 -0400

    dashboard fetches latest data

--- server/__init__.py
+++ server/__init__.py
@@ -1,8 +1,11 @@
+import asyncio
 from datetime import datetime
 import json
+import os
 
 import aiofiles
 from sanic import Request, Sanic
+from sanic.response import text, file
 from zoneinfo import ZoneInfo
 
 from poster.template import Renderable
@@ -13,7 +16,7 @@ from poster import posting_target
 
 from .auth import login_required, bp as auth_bp
 from .file_upload import bp as file_upload_bp
-from .report import bp as report_bp
+from .sync_logs import sync_logs, write_parquet
 
 app = Sanic("crossposter")
 app.config.TEMPLATING_PATH_TO_TEMPLATES = "./server/templates"
@@ -22,7 +25,6 @@ app.config.SECRET = secrets["SERVER_SECRET"]
 app.static("/assets", "./server/dist/assets", name="assets")
 app.static("/log_files", "./server/log_files", name="log_files")
 app.blueprint(file_upload_bp)
-app.blueprint(report_bp)
 app.blueprint(auth_bp)
 
 
@@ -112,11 +114,63 @@ async def dashboard(request: Request, username):
 
     if len(sites) == 0:
         ctx["error"] = "Not allowed to access any sites"
+        return ctx
     elif site is None:
         ctx["error"] = "Must specify a \"site\" argument"
+        return ctx
     elif site not in sites:
         ctx["error"] = "Not allowed to access that site"
+        return ctx
     else:
         ctx["site"] = site
 
+    await asyncio.to_thread(sync_logs, site)
+    await asyncio.to_thread(write_parquet, site)
+
     return ctx
+
+
+@app.get("/report")
+@login_required
+async def report(request: Request, username: str):
+    """
+    TODO: deprecate this in favor of `/dashboard`
+    """
+    sites = secrets["logs"].get(username, [])
+    if len(sites) == 0:
+        return text("no sites for user", 400)
+
+    site_name = request.args.get("site", sites[0])
+    if site_name not in sites:
+        return text(f"site {site_name} not found", 400)
+
+    site = secrets.get(site_name, {}).get("logs", None)
+    if site is None:
+        return text(f"site {site} not defined", 500)
+
+    env = os.environ.copy()
+    for var in [
+        "AWS_ACCESS_KEY_ID",
+        "AWS_SECRET_ACCESS_KEY",
+        "AWS_REGION_NAME",
+        "AWS_BUCKET_NAME",
+    ]:
+        env[var] = site[var]
+
+    await asyncio.to_thread(sync_logs, site_name)
+
+    proc = await asyncio.create_subprocess_exec(
+        "bash",
+        "./server/gen_report.sh",
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE,
+        env=env,
+    )
+
+    stdout, stderr = await proc.communicate()
+
+    if proc.returncode != 0:
+        return text(stderr.decode(), 500)
+
+    filename = stdout.decode().strip()
+    return await file(filename)
