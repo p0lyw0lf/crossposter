1ffa8ec6c01c1256af0fdae120abf0ecf4b19fe9
PolyWolf <31190026+p0lyw0lf@users.noreply.github.com>
2025-05-06 20:48:46 -0400

    rc: add tags to preview

--- rc/web/src/Composer/Editor/Preview.tsx
+++ rc/web/src/Composer/Editor/Preview.tsx
@@ -1,5 +1,5 @@
 import type { Component } from "solid-js";
-import { createResource, Suspense } from "solid-js";
+import { createResource, For, Suspense } from "solid-js";
 import { useComposerContext } from "../ComposerContext";
 import { draftFromFormData } from "../drafts";
 import { render } from "../renderer";
@@ -11,12 +11,24 @@ export const Preview: Component = () => {
   const draft = draftFromFormData(new FormData(store.formRef));
 
   const markdown = () => (draft ? `# ${draft.title}\n\n${draft.body}` : null);
+  const tags = () => (draft ? draft.tags : []);
   const [html] = createResource(markdown, render);
 
   return (
-    <Suspense fallback={<div class={styles.preview}>Loading...</div>}>
+    <Suspense
+      fallback={
+        <div class={styles.preview}>
+          <p>Loading...</p>
+        </div>
+      }
+    >
       {/* eslint-disable-next-line solid/no-innerhtml */}
       <div class={styles.preview} innerHTML={html()} />
+      {tags().length && (
+        <div class={styles.tags}>
+          <For each={tags()}>{(tag) => <span>#{tag}</span>}</For>
+        </div>
+      )}
     </Suspense>
   );
 };
