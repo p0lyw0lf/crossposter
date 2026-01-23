eee11b81adff99cc63f01854e33b6a052f040dca
p0lyw0lf <31190026+p0lyw0lf@users.noreply.github.com>
2026-01-22 22:33:10 -0500

    add drafts file viewer
    next commit will be removing the composer stuff; splitting into two
    commits just in case I do really want to keep them around combined. I
    don't think I do right now but just in case u never know !

--- rc/src/rc/drafts.py
+++ rc/src/rc/drafts.py
@@ -2,23 +2,20 @@ from dataclasses import asdict
 from datetime import datetime
 from pathlib import Path
 import os
-from typing import Tuple
+import urllib.parse
 
 import aiofiles
 from sanic import Blueprint, Request
-from sanic.request import RequestParameters
-from sanic.response import json, text
+from sanic.response import text, file, redirect
 from sanic_ext import render
 import mistletoe
 
-from poster.model import Post, to_slug
-from poster.template import templates
+from poster.model import Post
 
-from .auth import login_required
 from .web import get_manifest
 
-data_dir = Path(os.environ.get("RC_DATA_DIR", os.path.dirname(os.path.realpath(__file__))))
-drafts_dir = data_dir / "drafts"
+data_dir = Path(os.environ.get("RC_DRAFTS_DIR", os.path.dirname(os.path.realpath(__file__))))
+drafts_dir = (data_dir / "drafts").resolve(strict=True)
 
 bp = Blueprint("drafts", url_prefix="/drafts")
 
@@ -28,126 +25,106 @@ async def post_from_file(p: Path) -> Post:
     Given a filename for a serialized post, returns that post as a Python object.
     """
     async with aiofiles.open(p, mode="r") as f:
-        contents = await f.read()
-    return Post.from_string(contents)
+        content = await f.read()
 
-
-async def post_from_form(form: RequestParameters) -> Post | None:
-    """
-    Given a Sanic form object, parse the Post out of it.
-    """
-    title = form.get("title")
-    body = form.get("body")
-    tags = form.getlist("tags")
-
-    if not (\
-        isinstance(title, str) and \
-        isinstance(body, str) and \
-        isinstance(tags, list) and \
-        all(isinstance(tag, str) for tag in tags) \
-    ):
-        return None
-
-    return Post(
-        title=title,
-        description=None,
-        tags=tags,
-        published=datetime.now(),
-        repost_link=None,
-        body=body,
-    )
-
-async def serialize_post_to_file(path: Path, post: Post):
-    """
-    Given a filename for a post object, serializes the post to that file.
-    """
-    rendered = await serialize_post(post)
-    async with aiofiles.open(path, mode="w") as f:
-        await f.write(rendered)
-
-async def serialize_post(post: Post) -> str:
-    template = templates["github_blog.md.j2"]
-    rendered = await template.render_async(
-        slug=to_slug(post),
-        **asdict(post),
-    )
-    return rendered
-
-def json_from_post(id: str, post: Post) -> object:
-    return {
-        "draftId": id,
-        "title": post.title,
-        "body": post.body,
-        "tags": post.tags,
-    }
-
-@bp.get("list")
-@login_required
-async def list_drafts(request: Request, username: str):
-    dir = drafts_dir / username
-    dir.mkdir(parents=True, exist_ok=True)
-
-    drafts: list[Tuple[str, Post]] = []
-    for f in dir.iterdir():
-        drafts.append((f.stem, await post_from_file(f)))
-    drafts.sort(key=lambda p: p[1].title)
-
-    return json([json_from_post(id, draft) for id, draft in drafts])
-
-@bp.get("by_id/<id:slug>")
-@login_required
-async def get_draft(request: Request, username: str, id: str):
-    """
-    This endpoint fetches the draft from disk, and returns it as a json object.
-    `/drafts/render/<id>` should be used to show the final product of a draft.
-    """
-
-    file = drafts_dir / username / f"{id}.md"
-    draft = await post_from_file(file)
-
-    return json(json_from_post(id, draft))
-
-@bp.put("by_id/<id:slug>")
-@login_required
-async def upsert_draft(request: Request, username: str, id: str):
-    print("handling upsert_draft")
-    if request.form is None:
-        return text("must provide form", status=400)
-
-    draft = await post_from_form(request.form)
-    if draft is None:
-        return text("bad post", status=400)
-
-    file = drafts_dir / username / f"{id}.md"
-    await serialize_post_to_file(file, draft)
-
-    return text("ok")
-
-@bp.delete("by_id/<id:slug>")
-@login_required
-async def delete_draft(request: Request, username: str, id: str):
-
-    file = drafts_dir / username / f"{id}.md"
-    file.unlink()
-
-    return text("ok")
-
-@bp.get("render/<username:slug>/<id:slug>")
-async def render_draft(request: Request, username: str, id: str):
-    """
-    This endpoint takes the draft markdown identified by the id and renders it as HTML. No login is
-    required, because these links are meant to be shared with other people for pre-post review.
-    """
-
-    file = drafts_dir / username / f"{id}.md"
     try:
-        post = await post_from_file(file)
+        # Try to parse out frontmatter
+        post = Post.from_string(content)
     except:
-        return text("Not found", status=400)
+        # In case there is no frontmatter, treat the entire thing as markdown
+        post = Post(
+            title="<Untitled>",
+            description=None,
+            tags=[],
+            published=datetime.now(),
+            repost_link=None,
+            body=content,
+        )
 
+    return post
+
+@bp.get("/<subdir:path>")
+@bp.get("/", name="get_drafts_index")
+async def get_drafts(request: Request, subdir=None):
+    """
+    This endpoint shows a simple file explorer for the drafts directory, rendering any ".md" files into HTML.
+    No login is required because these links are meant to be shared.
+    """
+    path = drafts_dir
+    if subdir:
+        subdir = urllib.parse.unquote(subdir)
+        path = path / subdir
+
+    try:
+        print(path)
+        path = path.resolve(strict=True)
+        if not path.is_relative_to(drafts_dir):
+            return text("hahaha no", status=400)
+    except:
+        return text("Could not resolve path", status=404)
+
+    if not path.exists():
+        return text("Path doesn't exist", status=404)
+    elif path.is_dir():
+        if subdir is not None and not subdir.endswith("/"):
+            return redirect(f"/drafts/{subdir}/")
+        return await list_dir(path)
+    elif path.is_file():
+        if path.suffix == ".md":
+            return await render_file(path)
+        else:
+            return await file(path)
+    else:
+        return text("Bad inode", status=400)
+
+
+async def list_dir(path: Path):
+    """
+    Given a directory at `path`, returns an HTML page.
+    REQUIRES: path.exists() and path.is_dir()
+    """
+    entries = []
+    for f in path.iterdir():
+        if f.is_dir():
+            entries.append({
+                "type": "dir",
+                "name": f.name,
+            })
+        elif f.is_file():
+            title = f.name
+            if f.suffix == ".md":
+                try:
+                    post = await post_from_file(f)
+                    title = post.title
+                except:
+                    pass
+            title = title or "<Untitled>"
+            entries.append({
+                "type": "file",
+                "title": title,
+                "name": f.name,
+            })
+
+    entries.sort(key=lambda e: e["type"] + e["name"])
+    print(path, entries)
+    return await render(
+        "drafts/dir.html.j2",
+        context={
+
+            "index": await get_manifest("Render"),
+            "entries": entries,
+        },
+    )
+
+async def render_file(path: Path):
+    """
+    Given a markdown file at `path`, renders it as HTML.
+    REQUIRES: path.exists() and path.is_file()
+    """
+    post = await post_from_file(path)
     body = mistletoe.markdown(post.body)
     return await render(
-        "render.html.j2",
+        "drafts/file.html.j2",
         context={
             "index": await get_manifest("Render"),
             "body": body,
